fae76b00787c253c7a79c562420796e1dcda6f4a
CharmilaGit <charmilavenum@gmail.com>
2025-01-29 22:36:06 +0530

    Add files via upload

---
+++ deployy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+docker-compose up -d
+docker login -u ar8888  -p dckr_pat_NqD6cUw-P2zyV9GQ715-Y8hLK_I
+
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+       sh'chmod +x build.sh'
+       sh'./build.sh'
+
+        docker tag test ar8888/dev
+        docker push charmilavenum/dev
+
+if [[ $GIT_BRANCH == "origin/master" ]]; then
+        sh'chmod +x build.sh'
+        sh'./build.sh'
+
+        docker tag test ar8888/prod
+        docker push charmilavenum/prod
+
+else
+        echo "failed"
+fi
